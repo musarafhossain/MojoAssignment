3f88fb591af0b72d125a11298b520b73d2a65fe2
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2024-09-06 23:37:58 +0530

    add login code

--- app/page.js
+++ app/page.js
@@ -1,7 +1,16 @@
-import Image from "next/image";
+"use client"
+import { useSession, signIn, signOut } from "next-auth/react"
 
-export default function Home() {
-  return (
-    <div>hello</div>
-  );
+export default function Component() {
+  const { data: session } = useSession()
+  if(session) {
+    return <>
+      Signed in as {session.user.email} <br/>
+      <button onClick={() => signOut()}>Sign out</button>
+    </>
+  }
+  return <>
+    Not signed in <br/>
+    <button onClick={() => signIn()}>Sign in</button>
+  </>
 }
